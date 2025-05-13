04db5bb31bd069c42c472a67dad69d5a5e959f7e
dpalacios <palaciosdanieldario@gmail.com>
2025-05-12 22:52:09 -0500

    Edited main.c

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * FEATURE #1 Host URL & Port Number
+ *
+ * - Users should have the ability to visit application on any device with internet.
+ * - User Log in is required
+ * 
+ * TODO 
+ *  
+ * - Host HTML file on reliable socket
+ * - Obtain Certification to host locally & production
+ * - Create Admin Page & keep track of users
+ * - Store User information in SQL database (stretch goal)
+ *
+ * - Start Date: 2025-05-13
+ *
+ * - Due Date: 2025-05-24
+ *
+ *
+ * */
 
 int main(){
 	return 0;
-
 }
