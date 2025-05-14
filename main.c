248f9b980b3ea4a63978a1eacfcee8884aa5d58c
dpalacios <palaciosdanieldario@gmail.com>
2025-05-13 21:32:55 -0500

    Added Header and output files for socket configurations

--- main.c
+++ main.c
@@ -2,11 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <poll.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include "socket.h"
+
 
 /*
  * FEATURE #1 Host URL & Port Number
@@ -25,11 +22,16 @@
  *
  * - Due Date: 2025-05-24
  *
- *
  * */
 
 
 int main(){
-	
+	struct pollfd *pfds;
+	int max_fd_size = 10;
+	int fd_count = 0;
+	pfds = malloc(sizeof(struct pollfd) * max_fd_size);
+	int listener_socket = create_socket();
+	add_fd(listener_socket, pfds, fd_count, max_fd_size);
+	listen_for_pfds(pfds, fd_count, max_fd_size);
 	return 0;
 }
---
+++ socket.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 9034
+int create_socket(){
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	
+	int listener_socket = socket(AF_INET, SOCK_STREAM, 0);
+	int yes = 1;
+
+	setsockopt(listener_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+	bind(listener_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+	listen(listener_socket, 5);
+	return listener_socket;
+
+}
+
+
+void add_fd(int new_fd, struct pollfd *pfds, int fd_count, int max_fd_size){
+	if (fd_count == max_fd_size){
+		max_fd_size *=2;
+		pfds = realloc(pfds, sizeof(pfds) * max_fd_size);
+	}
+	pfds[fd_count].fd = new_fd;
+	pfds[fd_count].events = POLLIN;
+	fd_count++;
+	printf("New File Descriptor Added: %d\n", new_fd);
+	
+}
+int get_ready_file_descriptor(int fd_count, struct pollfd *pfds){
+	for (int i=0; i<fd_count; i++){
+		if (pfds[i].revents & POLLIN){
+			return pfds[i].fd;
+		}
+	}
+}
+
+void listen_for_pfds(struct pollfd *pfds, int fd_count, int max_fd_size){
+	printf("https://127.0.0.1:%d\n",PORT );
+	while(1){
+		printf("Listening...\n");
+		if (poll(pfds, fd_count, -1) < 0){
+			perror("poll");
+		}
+	}
+
+
+}
+
+
