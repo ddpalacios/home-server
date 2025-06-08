190beb9ce2c2c5242a62e616bf10673d8ce6280d
dpalacios <palaciosdanieldario@gmail.com>
2025-06-08 00:33:57 -0500

    Cmake compiler - major project changes on scaling

---
+++ build/CMakeFiles/home-server.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dpalacios/home-server/database/SQL.c" "CMakeFiles/home-server.dir/database/SQL.c.o" "gcc" "CMakeFiles/home-server.dir/database/SQL.c.o.d"
+  "/home/dpalacios/home-server/main.c" "CMakeFiles/home-server.dir/main.c.o" "gcc" "CMakeFiles/home-server.dir/main.c.o.d"
+  "/home/dpalacios/home-server/models/User.c" "CMakeFiles/home-server.dir/models/User.c.o" "gcc" "CMakeFiles/home-server.dir/models/User.c.o.d"
+  "/home/dpalacios/home-server/models/session.c" "CMakeFiles/home-server.dir/models/session.c.o" "gcc" "CMakeFiles/home-server.dir/models/session.c.o.d"
+  "/home/dpalacios/home-server/models/websocket.c" "CMakeFiles/home-server.dir/models/websocket.c.o" "gcc" "CMakeFiles/home-server.dir/models/websocket.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/DELETE/sessioninfo.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/DELETE/sessioninfo.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/DELETE/sessioninfo.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/GET/home.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/home.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/home.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/GET/login.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/login.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/login.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/GET/new_login.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/new_login.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/new_login.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/GET/sessioninfo.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/sessioninfo.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/sessioninfo.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/GET/studio.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/studio.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/studio.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/GET/users.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/users.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/users.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/GET/websocket_protocol.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/websocket_protocol.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/GET/websocket_protocol.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/PATCH/patch_websocket.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/PATCH/patch_websocket.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/PATCH/patch_websocket.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/POST/login.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/POST/login.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/POST/login.c.o.d"
+  "/home/dpalacios/home-server/routes/life-of-sounds/POST/new_user.c" "CMakeFiles/home-server.dir/routes/life-of-sounds/POST/new_user.c.o" "gcc" "CMakeFiles/home-server.dir/routes/life-of-sounds/POST/new_user.c.o.d"
+  "/home/dpalacios/home-server/routes/route.c" "CMakeFiles/home-server.dir/routes/route.c.o" "gcc" "CMakeFiles/home-server.dir/routes/route.c.o.d"
+  "/home/dpalacios/home-server/server/client.c" "CMakeFiles/home-server.dir/server/client.c.o" "gcc" "CMakeFiles/home-server.dir/server/client.c.o.d"
+  "/home/dpalacios/home-server/server/socket.c" "CMakeFiles/home-server.dir/server/socket.c.o" "gcc" "CMakeFiles/home-server.dir/server/socket.c.o.d"
+  "/home/dpalacios/home-server/utilities/http_utilities.c" "CMakeFiles/home-server.dir/utilities/http_utilities.c.o" "gcc" "CMakeFiles/home-server.dir/utilities/http_utilities.c.o.d"
+  "/home/dpalacios/home-server/utilities/json_utilities.c" "CMakeFiles/home-server.dir/utilities/json_utilities.c.o" "gcc" "CMakeFiles/home-server.dir/utilities/json_utilities.c.o.d"
+  "/home/dpalacios/home-server/utilities/string_utilities.c" "CMakeFiles/home-server.dir/utilities/string_utilities.c.o" "gcc" "CMakeFiles/home-server.dir/utilities/string_utilities.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/home-server.dir/routes/life-of-sounds/GET/new_user_page.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/home-server.dir/routes/life-of-sounds/GET/new_user_page.c.o: \
+ /home/dpalacios/home-server/routes/life-of-sounds/GET/new_user_page.c \
+ /usr/include/stdc-predef.h
