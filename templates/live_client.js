1fc342a4e4ab41ab91ea7eaa83982aa98ea2a4e5
dpalacios <palaciosdanieldario@gmail.com>
2025-07-30 18:19:52 -0500

    UI CHanges

--- templates/live_client.js
+++ templates/live_client.js
@@ -9,12 +9,12 @@ const dpr = window.devicePixelRatio || 1;
 canvas.width = width * dpr;
 canvas.height = height * dpr;
 ctx.scale(dpr, dpr);
-cellSize = 2
+pixel_size = 10
 let grid_cords = [];
 let mouse_cords = []
 var prev_cords = []
-var numRows =  Math.floor(canvas.height / cellSize);
-var numCols = Math.floor(canvas.width / cellSize);
+var numRows =  Math.floor(canvas.height / pixel_size);
+var numCols = Math.floor(canvas.width / pixel_size);
 
 function mouseMove(e){
 	e.preventDefault();
@@ -23,6 +23,7 @@ function mouseMove(e){
 	websocket_session.send(JSON.stringify({"type": 'mousemove', "x": cPostX, "y": cPostY }));
 
 }
+/*
 setInterval(() => {
 	for (let i = 0; i < mouse_cords.length; i++) {
 		const x = mouse_cords[i].x;
@@ -31,6 +32,7 @@ setInterval(() => {
 	}
 	mouse_cords = [];
 }, 2000);
+*/
 
 var image_w = 500;
 var image_h = 500;
@@ -39,48 +41,46 @@ async function start_websocket(){
 	websocket_session.onopen = () => {
 		console.log("Websocket connection established");	
 		canvas.addEventListener("mousemove", mouseMove, false);
-		ctx.beginPath()
-		ctx.strokeStyle = "red"
-		ctx.rect(0,0, image_w,image_h);
-		ctx.stroke();
-		ctx.closePath();
 	}
 
 	websocket_session.onmessage = (event) => {
 				var cords = JSON.parse(event.data)
-		 		console.log(cords);
 				if (cords['type'] == 'live'){
-					for (var i=0; i<grid_cords.length; i++){
-							var x = grid_cords[i]['x']
-							var y = grid_cords[i]['y']
-							ctx.clearRect(x, y, 50, 50);
-						}
 
-					grid_cords = [];
-					console.log(cords['cords'])
-					for (var i =0; i<cords['cords'].length; i++){
-						if (i >= 1){break;}
-						var x = cords['cords'][i].x;
-						var y = cords['cords'][i].y;
-						ctx.fillStyle = "black"
-						ctx.fillRect(x,y,50,50);
-						grid_cords.push({'x': x, 'y': y})
+					 for (let i =0; i<numRows; i++){
+						 grid[i] = [];
+						 for (let j=0; j<numCols; j++){
+							  var x = j * pixel_size;
+							  var y = i * pixel_size
+							  grid[i][j] = 0;
+						 }
+					 }
+					console.log(cords['cords'].length);
+					if (grid.length > 0){
+						var r = cords['cords']
+						for (let i=0; i<r.length; i++){
+							var val = r[i];
+							var x = val['x']
+							var y = val['y']
+							grid[y][x] = 1;
+						}	
 					}
 				}
 
+
 				if (cords['type'] == 'mousemove'){
-					var cellSize = 20
+					var pixel_size = 20
 					for (var i=0; i<mouse_cords.length; i++){
 							var x = mouse_cords[i]['x']
 							var y = mouse_cords[i]['y']
-							ctx.clearRect(x, y, cellSize, cellSize);
+							ctx.clearRect(x, y, pixel_size, pixel_size);
 						}
 					mouse_cords = [];
 
 					var m_x = cords['x']
 					var m_y = cords['y']
 					ctx.fillStyle ="red"; 
-					ctx.fillRect(m_x,m_y ,cellSize, cellSize)
+					ctx.fillRect(m_x,m_y ,pixel_size, pixel_size)
 					mouse_cords.push({'x': m_x, 'y': m_y})
 					console.log(mouse_cords.length)
 						
@@ -97,6 +97,46 @@ async function start_websocket(){
 
 	}			
 }
+
+
+
+function mainLoop(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	 if (grid.length ==  0){
+		 for (let i =0; i<numRows; i++){
+			 grid[i] = [];
+			 for (let j=0; j<numCols; j++){
+				  var x = j * pixel_size;
+				  var y = i * pixel_size
+				  grid[i][j] = 0;
+			 }
+		 }
+
+	 }else{
+
+		 for (let i =0; i<numRows; i++){
+			 for (let j=0; j<numCols; j++){
+				  var x = j * pixel_size;
+				  var y = i * pixel_size;
+				  if (grid[i][j] == 1){
+					  ctx.fillStyle = "rgba(255,0,0,255)";
+
+
+				  }else{
+					  ctx.fillStyle = "rgba(0,0,0,0.9)";
+				  }
+
+
+				  ctx.fillRect(x + 1, y + 1, pixel_size, pixel_size);
+
+			 }
+		 }
+	 }
+	 requestAnimationFrame(mainLoop);
+}
+
+
 start_websocket()
+mainLoop();
 
                   
