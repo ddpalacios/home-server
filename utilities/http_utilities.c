94deff432c7d3f798b5367478239fd6dcf803f0f
dpalacios <palaciosdanieldario@gmail.com>
2025-10-10 10:41:31 -0500

    Changes

--- utilities/http_utilities.c
+++ utilities/http_utilities.c
@@ -136,35 +136,35 @@ char *get_file_buffer(char* filename) {
 char* open_html_template_page(char*template_name, char* request){
 	char* request_cookie = get_cookie(request);
 	char template_dir[50] = "../templates/";
-
-	if (request_cookie == NULL && strstr(template_name, "index.html") == NULL && strstr(template_name, "new_login.html") == NULL){
-		template_name = "index.html";
-		strcat(template_dir, template_name);
-
-		char *html_buffer = get_file_buffer(template_dir);
-		return html_buffer;
-	}else if ( strstr(template_name, "new_login.html") != NULL){
-				strcat(template_dir, template_name);
-				char *html_buffer = get_file_buffer(template_dir);
-				return html_buffer;
-	}else if ( strstr(template_name, "index.html") != NULL){
-				strcat(template_dir, template_name);
-				char *html_buffer = get_file_buffer(template_dir);
-				return html_buffer;
-	}else{
-		struct Session session = get_session(request_cookie);
-		if (!session.exists){
-				template_name = "index.html";
-				strcat(template_dir, template_name);
-				char *html_buffer = get_file_buffer(template_dir);
-				return html_buffer;
-		}else{
-				strcat(template_dir, template_name);
-				char *html_buffer = get_file_buffer(template_dir);
-				return html_buffer;
-			}
-	}
+	strcat(template_dir, template_name);
+	char *html_buffer = get_file_buffer(template_dir);
+	return html_buffer;
 }
+
+	// if (request_cookie == NULL && strstr(template_name, "index.html") == NULL && strstr(template_name, "new_login.html") == NULL){
+	// 	template_name = "index.html";
+	// 	strcat(template_dir, template_name);
+
+	// 	char *html_buffer = get_file_buffer(template_dir);
+	// 	return html_buffer;
+	// }else if ( strstr(template_name, "new_login.html") != NULL){
+	// 			strcat(template_dir, template_name);
+	// 			char *html_buffer = get_file_buffer(template_dir);
+	// 			return html_buffer;
+	// }else if ( strstr(template_name, "index.html") != NULL){
+	// 			strcat(template_dir, template_name);
+	// 			char *html_buffer = get_file_buffer(template_dir);
+	// 			return html_buffer;
+	// }else{
+	// 	struct Session session = get_session(request_cookie);
+	// 	if (!session.exists){
+	// 			template_name = "index.html";
+	// 			strcat(template_dir, template_name);
+	// 			char *html_buffer = get_file_buffer(template_dir);
+	// 			return html_buffer;
+	// 	}else{
+			
+	// }
 	
 
 
